41798576cfe816e488569bfcac9004fa3a97281c
mohamedgalalelhamrawy <mo.galal200012@gmail.com>
2024-03-12 14:42:05 +0200

    create splach view body

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const bookly() );
+}
+
+
+class bookly extends StatelessWidget {
+  const bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

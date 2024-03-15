941e873375071fee4f60b285fe9499919d9684a7
mohamedgalalelhamrawy <mo.galal200012@gmail.com>
2024-03-15 15:09:26 +0200

    create book details view

---
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:flutter/material.dart';
+
+class bookDetailsViewBody extends StatelessWidget {
+  const bookDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

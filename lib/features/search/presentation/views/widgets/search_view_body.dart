6574b965ac48c3b2aaef5359e8f793261246892a
mohamedgalalelhamrawy <mo.galal200012@gmail.com>
2024-03-17 15:56:54 +0200

    add search result list view to search view body

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,5 +1,6 @@
 
-
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/best_seller_listview_item.dart';
 import 'package:bookly_app/features/search/presentation/views/widgets/custom_search_textfield.dart';
 import 'package:flutter/material.dart';
 class searchViewBody extends StatelessWidget {
@@ -7,11 +8,37 @@ class searchViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [ 
-        SizedBox(height: 30,),
-        customSearchTextField(),
-      ],
+    return const Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [ 
+          SizedBox(height: 30,),
+          customSearchTextField(),
+           SizedBox( height: 16,),
+            Text("Search Result",style: styles.TextStyle18,),
+            SizedBox( height: 16,),
+              searchResultListView(),
+        ],
+      ),
+    );
+  }
+}
+
+class searchResultListView extends StatelessWidget {
+  const searchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Expanded(
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        itemCount: 10,
+        itemBuilder: (context,index){
+          return const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: BestSellerListViewItem(),
+          );
+        }),
     );
   }
 }
